48ab0df8fa5814a81fdd9ef6db04195d386c4eaf
shaco <209222581+shwaco@users.noreply.github.com>
2026-03-16 02:07:22 +0800

    feature: add a responsive sidebar

---
+++ dashboard/tourist/script.js
@@ -0,0 +1,10 @@
+// Sidebar
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
