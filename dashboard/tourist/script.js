9da8b17b7b597b31ab1f88b278a83473d6dccb78
shaco <209222581+shwaco@users.noreply.github.com>
2026-03-24 09:45:32 +0800

    feat: add sliding interactivity

--- dashboard/tourist/script.js
+++ dashboard/tourist/script.js
@@ -8,3 +8,39 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+// Image slider 1
+const track =  document.querySelector('.slider1 ul');
+const prevBtn = document.getElementById('prev-btn1');
+const nextBtn = document.getElementById('next-btn1');
+
+function getScrollAmount() {
+    const itemWidth = track.querySelector('li').clientWidth;
+    const gap = 16;
+    return itemWidth + gap;
+}
+
+nextBtn.addEventListener('click', () => {
+    track.scrollBy({ left: getScrollAmount(), behavior: 'smooth' })
+    });
+
+prevBtn.addEventListener('click', () => {
+    track.scrollBy({ left: -getScrollAmount(), behavior: 'smooth'})
+})
+
+function updateButtonVisibility () {
+    if (track.scrollLeft <= 0) {
+        prevBtn.style.display = 'none';
+    } else {
+        prevBtn.style.display = 'flex';
+    }
+    if (track.scrollLeft + track.clientWidth >= track.scrollWidth - 1) {
+        nextBtn.style.display = 'none';
+    } else {
+        nextBtn.style.display = 'flex';
+    }
+}
+
+track.addEventListener('scroll', updateButtonVisibility);
+
+updateButtonVisibility();
