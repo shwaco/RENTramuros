cd7597114ec1bde7896b1af4bce380cea2b02b20
keyelzzz <keyelmc@gmail.com>
2026-03-31 18:27:06 +0800

    pgit

---
+++ inclusions/cancel_bookings.php
@@ -0,0 +1,26 @@
+<?php
+include 'db_connect.php';
+
+if(isset($_GET['reservation_id'])) {
+    
+    $reservation_id = $_GET['reservation_id'];
+
+    $sql = "UPDATE RESERVATIONS
+            SET status = 'Cancelled'
+            WHERE reservation_id = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $reservation_id);
+
+    if($stmt->execute()) {
+        echo "Booking Cancelled Successfully!";
+    }
+    else {
+        echo "Error Cancelling Booking.";
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+?>
---
+++ inclusions/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = " "; 
+$database = "rentramuros_database";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
