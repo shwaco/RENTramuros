52b8ebba50139342dc5323df5a8e7220802d943d
shaco <209222581+shwaco@users.noreply.github.com>
2026-04-16 15:38:54 +0800

    refactor: pop attractions and reco attractions

--- services/api.js
+++ services/api.js
@@ -1,18 +1,37 @@
-// retrieve recommended attractions 
-async function getRecommendedAttractions() {
+// retrieve popular attractions 
+async function getPopularAttractions() {
     try {
         const response = await fetch('/RENTramuros/backend/attractions/retrieve_attractions.php');
 
         if (!response.ok) {
             throw new Error (`Response status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        console.log(result);
+        } else {
+            const result = await response.json();
+            return result.data || [];
+        } 
 
     } catch (error) {
         console.error(error.message);
+        return [];
     } 
 }
 
-document.addEventListener('DOMContentLoaded', getRecommendedAttractions);
+// retrieve recommended attractions
+async function getRecommendedAttractions {
+    try {
+        const response = await fetch('/RENTramuros/backend/attractions/PHPFILE');
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        } else {
+            const result = response.json();
+            return result.data || [];
+        }
+
+    } catch (error) {
+        console.error(error.message);
+        return [];
+    }
+} 
+
+document.addEventListener('DOMContentLoaded', getPopularAttractions);
