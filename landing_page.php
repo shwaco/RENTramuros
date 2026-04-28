67e1c7424002793f4c4fdbd0179b1914c6730a4e
shaco <209222581+shwaco@users.noreply.github.com>
2026-04-28 12:08:37 +0800

    fix: rename and modular navsidebar

--- landing_page.php
+++ landing_page.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght,XOPQ,XTRA,YOPQ,YTDE,YTFI,YTLC,YTUC@8..144,100..1000,96,468,79,-203,738,514,712&family=Roboto+Slab:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="asset/css/navbar.css?v=<?php echo filemtime('asset/css/navbar.css'); ?>"></link>
+    <link rel="stylesheet" href="asset/css/navsidebar.css?v=<?php echo filemtime('asset/css/navsidebar.css'); ?>"></link>
     <link rel="stylesheet" href="asset/main.css?v=<?php echo filemtime('asset/main.css'); ?>">
     <link rel="stylesheet" href="asset/css/index.css?v=<?php echo filemtime('asset/css/index.css'); ?>">
     <script type="module" src="asset/script.js?v=<?php echo filemtime('asset/script.js'); ?>" defer></script>
