c690741f7c74a479e442b0f5c32c3351f6bdf405
shaco <209222581+shwaco@users.noreply.github.com>
2026-04-29 23:43:16 +0800

    docu: re-pathing

--- auth.v2/backend/config/mailer_config.php
+++ auth.v2/backend/config/mailer_config.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../../PHPMailer/src/Exception.php';
-require '../../../PHPMailer/src/PHPMailer.php';
-require '../../../PHPMailer/src/SMTP.php';
+require '../../PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 try {
