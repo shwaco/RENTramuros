b1dbd7d5959dc5d896599c29aa5a74388acb8a52
shaco <209222581+shwaco@users.noreply.github.com>
2026-04-30 19:54:29 +0800

    map fix

---
+++ DashWithMap/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    
+    <link rel="stylesheet" href="style.css">
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+    <script src="main.js" defer></script>
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Intramuros Map</title>
+
+  </head>
+  <body>
+
+    
+</body>
+</html>
