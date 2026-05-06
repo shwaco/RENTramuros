936d90813b2f00d16e28bce43f3d9b196d1257ed
Nesto <arevaloernestojohn@gmail.com>
2026-05-07 07:17:04 +0800

    login design

--- pages/login_page/login.php
+++ pages/login_page/login.php
@@ -7,10 +7,13 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Imperial+Script&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght,XOPQ,XTRA,YOPQ,YTDE,YTFI,YTLC,YTUC@8..144,100..1000,96,468,79,-203,738,514,712&family=Roboto+Slab:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="login.css?v=<?php echo filemtime('login.css'); ?>">
+
     <script src="login.js?v=<?php echo filemtime('login.js'); ?>" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
 </head>
 <body>
     <div class="main_container">
@@ -18,23 +21,21 @@
             <form id="LoginForm">
                 <h1>Login</h1>
                 <div class="wrapper">
-                    <div class="email_wrapper">
-                        <input class="inputs email" type="email" id="emailInput" name="email" placeholder="Email" required>
-                    </div>
+                    <input class="inputs" type="email" id="emailInput" name="email" placeholder="Email" required>
                     <div class="password_wrapper">
-                        <input class="inputs pass" type="password" id="passwordInput" name="password" placeholder="Password" required>
+                        <input class="inputs" type="password" id="passwordInput" name="password" placeholder="Password" required>
                         <button type="button" id="pass_visibility">
                             <img id="visibility_icon" src="../../asset/img/pass_visibility.svg">
                         </button>
                     </div>
-                    <div class="login_wrapper">
-                        <button type="submit" id="submit_button">Login</button>
+                    <div class="sign_up_wrapper">
+                        <button type="submit" id="submit_button">LOGIN</button>
                     </div>
-                    <div id="login_message"></div>
+                    <div id="alertbox"></div>
                     <p class="login_footer">Don't have an account? <a href="../sign_up_page/sign_up.php">Sign up</a></p>
                 </div>
             </form>
         </div>
-        
-    </body>
+    </div>
+</body>
 </html>
